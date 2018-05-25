4e05f49d0b583da10a3e9cac2bd47f287def53e4
Cristina <cristinita@gmail.com>
2018-05-25 15:52:21 +0200

    P8_v1.1

--- controllers/tip.js
+++ controllers/tip.js
@@ -18,6 +18,45 @@ exports.load = (req, res, next, tipId) => {
 };
 
 
+//
+exports.adminOrAuthorRequired = (req, res, next) => {
+    const isAdmin =!! req.session.user.isAdmin;
+    const isAuthor = req.session.user.id === req.tip.authorId;
+    if(isAdmin || isAuthor){
+        next();
+    }else{
+        req.send(403);
+    }
+}
+
+// GET /quizzes/:quizId/edit
+exports.edit = (req, res, next) => {
+    const{quiz,tips} = req;
+    res.render('tips/edit', {quiz, tips});
+}
+
+// PUT /quizzes/:quizId
+exports.update = (req, res, next) => {
+    const{quiz,tips} = req;
+    tip.text = req.body.text;
+    tip.save({fields: ["text", "accepted"]})
+    .then(tips => {
+        req.flash('success', 'Tips edited successfully.');
+        res.redirect('/quizzes/' + quiz.id);
+    })
+    .catch(Sequelize.ValidationError, error => {
+        req.flash('error', 'There are errors in the form:');
+        error.errors.forEach(({message}) => req.flash('error', message));
+        res.render('tips/edit', {quiz});
+    })
+    .catch(error => {
+        req.flash('error', 'Error editing the Quiz: ' + error.message);
+        next(error);
+    });
+}
+
+
+
 // POST /quizzes/:quizId/tips
 exports.create = (req, res, next) => {
    //const(question, answer)=req.body;
@@ -75,4 +114,6 @@ exports.destroy = (req, res, next) => {
     })
     .catch(error => next(error));
 };
+
+
 
--- routes/index.js
+++ routes/index.js
@@ -139,4 +139,14 @@ router.get('/quizzes/randomplay', quizController.randomPlay);
 router.get('/quizzes/randomcheck/:quizId(\\d+)', quizController.randomCheck);
 
 
+
+router.get('/quizzes/:quizId(\\d+)/tips/:tipId(\\d+)/edit',
+    sessionController.loginRequired,
+    quizController.adminOrAuthorRequired,
+    tipController.edit);
+router.put('/quizzes/:quizId(\\d+)/tips/:tipId(\\d+)',
+    sessionController.loginRequired,
+    quizController.adminOrAuthorRequired,
+    quizController.update);
+
 module.exports = router;
